4672d27564751ba0c6b2c9c735e1bf750dbcc6c7
sidonie <shelliednnguyen@gmail.com>
2021-01-30 09:59:35 -0800

    fetched and parsed Storm Glass data

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+let parseStormglassData = function( fetchedData ) {
+   let airTemp = fetchedData.hours[ 0 ].airTemperature.noaa;
+   // Convert from celcius to fahrenheit and round to 2 decimal places
+   airTemp = (( airTemp * ( 9 / 5 )) + 32 ).toFixed( 2 );
+   console.log( "airTemp: " + airTemp + " °F");
+
+   let cloudCover = fetchedData.hours[ 0 ].cloudCover.noaa;
+   console.log( "cloudCover: " + cloudCover + "%" );
+
+   let humidity = fetchedData.hours[ 0 ].humidity.noaa;
+   console.log( "humidity: " + humidity + "%" );
+
+   let swellDirection = fetchedData.hours[ 0 ].swellDirection.noaa;
+   console.log( "swellDirection: " + swellDirection + "°" );
+
+   let swellPeriod = fetchedData.hours[ 0 ].swellPeriod.noaa;
+   console.log( "swellPeriod: " + swellPeriod + "s" );
+
+   let waveDirection = fetchedData.hours[ 0 ].waveDirection.noaa;
+   console.log( "waveDirection: " + waveDirection + "°" );
+
+   let waveHeight = fetchedData.hours[ 0 ].waveHeight.noaa;
+   // Convert waveHeight from meters to feet and round to 2 decimal places
+   waveHeight = ( waveHeight * 3.281 ).toFixed( 2 );
+   console.log( "waveHeight: " + waveHeight + "ft" );
+
+   let wavePeriod = fetchedData.hours[ 0 ].wavePeriod.noaa;
+   console.log( "wavePeriod: " + wavePeriod + "s" );
+
+   let windDirection = fetchedData.hours[ 0 ].windDirection.noaa;
+   console.log( "windDirection: " + windDirection + "°" );
+
+   let windSpeed = fetchedData.hours[ 0 ].windSpeed.noaa;
+   // Convert waveHeight from meters to feet and round to 2 decimal places
+   windSpeed = ( windSpeed * 3.281 ).toFixed( 2 );
+   console.log( "windSpeed: " + windSpeed + "fts" );
+};
+
+////////////////////////////////////
+
+let fetchStormglassData = function() {
+   console.log( "today: " + new Date() );
+   let todayInUtcTime = Math.floor(( new Date().getTime()) / 1000 );
+   console.log( "todayInUtcTime: " + todayInUtcTime );
+
+   const lat = 33.5705796;
+   const lng = -117.8108887;
+   const params = "windSpeed,windDirection,cloudCover,humidity,airTemperature,waveHeight,waveDirection,wavePeriod,swellDirection,swellHeight,swellPeriod";
+   apiKey = "777e70f8-6125-11eb-83d4-0242ac130002-777e7170-6125-11eb-83d4-0242ac130002"
+
+   fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}&start=${todayInUtcTime}&end=${todayInUtcTime}&source=noaa`, {
+         headers: {
+         'Authorization': apiKey
+      }
+   }).then(( response ) => response.json()).then(( jsonData ) => {
+      // Do something with response data.
+      console.log( jsonData );
+      parseStormglassData( jsonData );
+   });
+};
+
+////////////////////////////////////
+
+// Upon page load
+$( document ).ready( function() {
+   fetchStormglassData();
+});
