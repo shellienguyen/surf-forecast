13c2cb3407f7c569466fba629cbfbd77955f4329
Software <55256368+Soft-Devel@users.noreply.github.com>
2021-01-31 13:21:54 -0800

    finished gifs integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,87 @@ let parseStormglassData = function( fetchedData ) {
    console.log( "windSpeed: " + windSpeed + "fts" );
 };
 
+
+// Fetch Gifs from Giphy
+
+let fetchGifs = function( fetchedData ) {
+
+   let windData = fetchedData.hours [ 0 ].windSpeed.noaa;
+
+   console.log("my wind data is " + windData);
+
+   let swellData = fetchedData.hours [ 0 ].swellHeight.noaa;
+
+   console.log("my swell data is " + swellData);
+
+   let waveData = fetchedData. hours [ 0 ].waveHeight.noaa;
+
+   console.log("my wave data is" + waveData);
+
+   let waveRatio = swellData / waveData ;
+
+   console.log(waveRatio);
+
+   if (windData >= 8.2) {
+
+      surfConditions = "windy surf";
+
+   } else if (windData > 6.69 && windData < 8.2 && waveRatio < .5) {
+
+      surfConditions = "bad surf";
+
+   } else if (waveData < .6) {
+
+      surfConditions = "small surf"
+
+   } else if (waveData > .6 && windData < 6.69 && waveRatio > .5) {
+      
+      surfConditions = "good surf";
+
+   }
+
+
+
+   console.log(surfConditions);
+
+   let off = Math.floor(Math.random() * 10 + 1);
+   console.log(off);
+
+   fetch(
+      `https://api.giphy.com/v1/gifs/search?q=' +
+        ${surfConditions} +
+        '&api_key=D4UdLTPgdxcfFYoHduwg3ZwTU5udC9K6&limit=1&offset=${off}`
+   )
+
+      .then(function(response) {
+         return response.json();
+      })
+      .then(function(response) {
+         console.log(response.data[0]);
+         let responseContainerEl = document.querySelector('#giphy');
+
+         responseContainerEl.innerHTML = "";
+
+         let gifImg = document.createElement('img');
+         gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
+
+         responseContainerEl.appendChild(gifImg);
+
+      });
+
+      let conditionsHeadingEl = document.querySelector("#surf-description");
+
+      let conditionsDescription = conditionsHeadingEl.innerHTML = surfConditions;
+
+      conditionsHeadingEl.appendChild(conditionsDescription);
+   
+
+}
+
+
+
 ////////////////////////////////////
+
 
 let fetchStormglassData = function() {
    console.log( "today: " + new Date() );
@@ -46,7 +126,7 @@ let fetchStormglassData = function() {
    const lat = 33.5705796;
    const lng = -117.8108887;
    const params = "windSpeed,windDirection,cloudCover,humidity,airTemperature,waveHeight,waveDirection,wavePeriod,swellDirection,swellHeight,swellPeriod";
-   apiKey = "777e70f8-6125-11eb-83d4-0242ac130002-777e7170-6125-11eb-83d4-0242ac130002"
+   apiKey = "e1969a56-6125-11eb-9cfc-0242ac130002-e1969ad8-6125-11eb-9cfc-0242ac130002"
 
    fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}&start=${todayInUtcTime}&end=${todayInUtcTime}&source=noaa`, {
          headers: {
@@ -56,7 +136,11 @@ let fetchStormglassData = function() {
       // Do something with response data.
       console.log( jsonData );
       parseStormglassData( jsonData );
+      fetchGifs(jsonData);
+
    });
+
+
 };
 
 ////////////////////////////////////
